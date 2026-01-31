51e71b4ecbd136781fc0b38d0de908a07680ba68
Shahriyor <nuriddinovlar20@gmail.com>
2026-01-31 17:04:26 +0500

    projects

--- src/pages/shop-info/index.tsx
+++ src/pages/shop-info/index.tsx
@@ -1,15 +1,12 @@
 import Feature from "../../components/features";
-import Footer from "../../components/footer";
-import Navbar from "../../components/navbar";
 import ShopInfoComponent from "../../components/shop-info";
 
 const ShopInfo = () => {
   return (
     <>
-      <Navbar />
+
       <ShopInfoComponent />
       <Feature/>
-      <Footer/>
     </>
   );
 };
